4b4f1ae66b9b532eca58259208cc509d9e9707c4
Adam Dad <adam-dad@hotmail.co.uk>
2018-12-31 14:51:57 +0000

    reduced footprint of code

--- naughts_and_crosses.cpp
+++ naughts_and_crosses.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
-#include <vector> // C++ array behaviour shocking without this.
 #include <string>
+#include <vector> // C++ array behaviour shocking without this.
 
 using namespace std;
 
-void draw_board();
-void draw_cross();
-void draw_naught();
-bool isloss();
+void draw_board(vector<string> board_squares);
+vector<string> draw_shape(string shape, vector<string> &board_squares);
+bool isloss(vector<string> &board_squares);
 
-vector<string> squares = {"[1]","[2]","[3]","[4]","[5]","[6]","[7]","[8]","[9]"};
 
 
 int main() {
+    vector<string> squares = {"[1]","[2]","[3]","[4]","[5]","[6]","[7]","[8]","[9]"};
+    string cross = "[X]";
+    string naught = "[O]";
+
     cout << "Let's play some naughts and crosses shall we?\n";
-    draw_board();
-    while (isloss()) {
-        draw_cross();
-        if (isloss() == false) {
+    draw_board(squares);
+
+    while (isloss(squares)) {
+        draw_shape(cross, squares);
+        if (!isloss(squares)) { // equivalent to if isloss == false
             cout << "Crosses Wins!\n";
             break;
         }
-        draw_naught();
-        if (isloss() == false) {
+        draw_shape(naught,squares);
+        if (!isloss(squares)) { // if not isloss
             cout << "Naughts Wins!\n";
             break;
         }
@@ -31,131 +34,90 @@ int main() {
 }
 
 
-void draw_board() {
+void draw_board(vector<string> board_squares) {
     string draw_board;
 
-    cout << squares[0] + squares[1] + squares[2] << "\n";   // OUTPUT:      [1][2][3]
-    cout << squares[3] + squares[4] + squares[5] << "\n";   //              [4][5][6]
-    cout << squares[6] + squares[7] + squares[8] << "\n";   //              [7][8][9]
+    cout << board_squares[0] + board_squares[1] + board_squares[2] << "\n";   // OUTPUT:      [1][2][3]
+    cout << board_squares[3] + board_squares[4] + board_squares[5] << "\n";   //              [4][5][6]
+    cout << board_squares[6] + board_squares[7] + board_squares[8] << "\n";   //              [7][8][9]
 }
 
 
-void draw_cross() {
-    string cross = "[X]";
+vector<string> draw_shape(string shape, vector<string> &board_squares) {
     int square_choice;
-    cout << "Pick a square, crosses\n";
+    cout << "Pick a square: ";
     cin >> square_choice;
 
     switch(square_choice) {
         case 1:
-            squares[0] = cross;
-            draw_board();
+            board_squares[0] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 2:
-            squares[1] = cross;
-            draw_board();
+            board_squares[1] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 3:
-            squares[2] = cross;
-            draw_board();
+            board_squares[2] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 4:
-            squares[3] = cross;
-            draw_board();
+            board_squares[3] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 5:
-            squares[4] = cross;
-            draw_board();
+            board_squares[4] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 6:
-            squares[5] = cross;
-            draw_board();
+            board_squares[5] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 7:
-            squares[6] = cross;
-            draw_board();
+            board_squares[6] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 8:
-            squares[7] = cross;
-            draw_board();
+            board_squares[7] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         case 9:
-            squares[8] = cross;
-            draw_board();
+            board_squares[8] = shape;
+            draw_board(board_squares);
+            return board_squares;
             break;
         default:
-            draw_board();
+            draw_board(board_squares);
+            return board_squares;
             break;
     }
 }
 
 
-void draw_naught() {
-    string naught = "[O]";
-    int square_choice;
-    cout << "Pick a square, naughts\n";
-    cin >> square_choice;
-
-    switch(square_choice) {
-        case 1:
-            squares[0] = naught;
-            draw_board();
-            break;
-        case 2:
-            squares[1] = naught;
-            draw_board();
-            break;
-        case 3:
-            squares[2] = naught;
-            draw_board();
-            break;
-        case 4:
-            squares[3] = naught;
-            draw_board();
-            break;
-        case 5:
-            squares[4] = naught;
-            draw_board();
-            break;
-        case 6:
-            squares[5] = naught;
-            draw_board();
-            break;
-        case 7:
-            squares[6] = naught;
-            draw_board();
-            break;
-        case 8:
-            squares[7] = naught;
-            draw_board();
-            break;
-        case 9:
-            squares[8] = naught;
-            draw_board();
-            break;
-        default:
-            draw_board();
-            break;
-    }
-}
-
-
-bool isloss() {
-    if (squares[0] == squares[1] && squares[1] == squares[2]) {
+bool isloss(vector<string> &board_squares) { // Possible winning moves listed here.
+    if (board_squares[0] == board_squares[1] && board_squares[1] == board_squares[2]) {
         return false;
-    } else if (squares[3] == squares[4] && squares[4] == squares[5]) {
+    } else if (board_squares[3] == board_squares[4] && board_squares[4] == board_squares[5]) {
         return false;
-    } else if (squares[6] == squares[7] && squares[7] == squares[8]) {
+    } else if (board_squares[6] == board_squares[7] && board_squares[7] == board_squares[8]) {
         return false;
-    } else if (squares[0] == squares[4] && squares[4] == squares[7]) {
+    } else if (board_squares[0] == board_squares[4] && board_squares[4] == board_squares[7]) {
         return false;
-    } else if (squares[2] == squares[4] && squares[4] == squares[6]) {
+    } else if (board_squares[2] == board_squares[4] && board_squares[4] == board_squares[6]) {
         return false;
-    } else if (squares[0] == squares[3] && squares[3] == squares[6]) {
+    } else if (board_squares[0] == board_squares[3] && board_squares[3] == board_squares[6]) {
         return false;
-    } else if (squares[1] == squares[4] && squares[4] == squares[7]) {
+    } else if (board_squares[1] == board_squares[4] && board_squares[4] == board_squares[7]) {
         return false;
-    } else if (squares[2] == squares[5] && squares[5] == squares[8]) {
+    } else if (board_squares[2] == board_squares[5] && board_squares[5] == board_squares[8]) {
         return false;
     } else {
         return true;
