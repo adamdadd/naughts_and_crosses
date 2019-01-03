49fa96d7af217147ed28cede5ae6e35fc863544a
Adam Dad <adam-dad@hotmail.co.uk>
2019-01-03 03:20:08 +0000

    bug fix: no more overwriting moves

--- naughts_and_crosses.cpp
+++ naughts_and_crosses.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <limits>
-#include <string>
 #include <vector> // C++ array behaviour shocking without this.
 
-using namespace std;
 
-void draw_board(vector<string> board_squares);
-vector<string> draw_shape(string shape, vector<string> &board_squares);
-bool isloss(vector<string> &board_squares);
-
+void draw_board(std::vector<std::string> board_squares);
+std::vector<std::string> draw_shape(std::string shape, std::vector<std::string> &board_squares);
+bool isloss(std::vector<std::string> &board_squares);
+bool check_schoice(int choice, std::vector<std::string> board_squares, std::string shape);
 
 
 int main() {
-    vector<string> squares = {"[1]","[2]","[3]","[4]","[5]","[6]","[7]","[8]","[9]"};
-    string cross = "[X]";
-    string naught = "[O]";
+    std::vector<std::string> squares = {"[1]","[2]","[3]","[4]","[5]","[6]","[7]","[8]","[9]"};
+    const std::string cross = "[X]";
+    const std::string naught = "[O]";
 
-    cout << "Let's play some naughts and crosses shall we?\n";
+    std::cout << "Let's play some naughts and crosses shall we?\n";
     draw_board(squares);
 
     while (isloss(squares)) {
-        cout << "Turn: CROSSES\n";
+        std::cout << "Turn: CROSSES\n";
         draw_shape(cross, squares);
+        draw_board(squares);
         if (!isloss(squares)) { // equivalent to if isloss == false
-            cout << "Crosses Wins!\n";
+            std::cout << "Crosses Wins!\n";
             break;
         }
-        cout << "Turn: NAUGHTS\n";
+        std::cout << "Turn: NAUGHTS\n";
         draw_shape(naught,squares);
+        draw_board(squares);
         if (!isloss(squares)) { // if not isloss
-            cout << "Naughts Wins!\n";
+            std::cout << "Naughts Wins!\n";
             break;
         }
     }
@@ -37,24 +37,38 @@ int main() {
 }
 
 
-void draw_board(vector<string> board_squares) {
-    string draw_board;
+void draw_board(std::vector<std::string> board_squares) {
+    std::string draw_board;
 
-    cout << board_squares[0] + board_squares[1] + board_squares[2] << "\n";   // OUTPUT:      [1][2][3]
-    cout << board_squares[3] + board_squares[4] + board_squares[5] << "\n";   //              [4][5][6]
-    cout << board_squares[6] + board_squares[7] + board_squares[8] << "\n";   //              [7][8][9]
+    std::cout << board_squares[0] + board_squares[1] + board_squares[2] << "\n";   // OUTPUT:      [1][2][3]
+    std::cout << board_squares[3] + board_squares[4] + board_squares[5] << "\n";   //              [4][5][6]
+    std::cout << board_squares[6] + board_squares[7] + board_squares[8] << "\n";   //              [7][8][9]
 }
 
 
-vector<string> draw_shape(string shape, vector<string> &board_squares) {
+bool check_schoice(int choice, std::vector<std::string> board_squares, std::string shape) {
+   if ((board_squares[choice] == "[X]") || (board_squares[choice] == "[O]")) {
+       std::cout << "\nAlready Occupied!\n";
+       std::cin.clear();
+       // empty buffer
+       std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+       return false;
+   } else {
+       return true;
+   }
+}
+
+
+std::vector<std::string> draw_shape(std::string shape, std::vector<std::string> &board_squares) {
     int square_choice;
     bool valid;
 
+
     do
     {
-        cout << "Pick a square: ";
-        cin >> square_choice;
-        if (cin.good())
+        std::cout << "Pick a square: ";
+        std::cin >> square_choice;
+        if (std::cin.good()) // If square_choice is a number
         {
             //everything went well, we'll get out of the loop and return the value
             valid = true;
@@ -62,61 +76,98 @@ vector<string> draw_shape(string shape, vector<string> &board_squares) {
         else
         {
             // reset the buffer's state to good
-            cin.clear();
+            std::cin.clear();
             // empty buffer
-            cin.ignore(numeric_limits<streamsize>::max(),'\n');
-            cout << "ERROR: That's not a number!" << endl;
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            std::cout << "ERROR: That's not a number!";
         }
     } while (!valid);
 
+
     switch(square_choice) {
         case 1:
-            board_squares[0] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(0, board_squares, shape)) {
+                board_squares[0] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 2:
-            board_squares[1] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(1, board_squares, shape)) {
+                board_squares[1] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 3:
-            board_squares[2] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(2, board_squares, shape)) {
+                board_squares[2] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 4:
-            board_squares[3] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(3, board_squares, shape)) {
+                board_squares[3] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 5:
-            board_squares[4] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(4, board_squares, shape)) {
+                board_squares[4] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 6:
-            board_squares[5] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(5, board_squares, shape)) {
+                board_squares[5] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 7:
-            board_squares[6] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(6, board_squares, shape)) {
+                board_squares[6] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 8:
-            board_squares[7] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(7, board_squares, shape)) {
+                board_squares[7] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
         case 9:
-            board_squares[8] = shape;
-            draw_board(board_squares);
-            return board_squares;
+            if (check_schoice(8, board_squares, shape)) {
+                board_squares[8] = shape;
+                return board_squares;
+            } else {
+                draw_shape(shape, board_squares);
+                return board_squares;
+            }
             break;
-        default:
-            cout << "ERROR: Not on the board, try again!\n";
+        default: // If anything other than a number from 1 to 9 is entered.
+            std::cout << "ERROR: Not on the board, try again!\n";
             draw_shape(shape,board_squares);
             return board_squares;
             break;
@@ -124,14 +175,14 @@ vector<string> draw_shape(string shape, vector<string> &board_squares) {
 }
 
 
-bool isloss(vector<string> &board_squares) { // Possible winning moves listed here.
+bool isloss(std::vector<std::string> &board_squares) { // Possible winning moves listed here.
     if (board_squares[0] == board_squares[1] && board_squares[1] == board_squares[2]) {
         return false;
     } else if (board_squares[3] == board_squares[4] && board_squares[4] == board_squares[5]) {
         return false;
     } else if (board_squares[6] == board_squares[7] && board_squares[7] == board_squares[8]) {
         return false;
-    } else if (board_squares[0] == board_squares[4] && board_squares[4] == board_squares[7]) {
+    } else if (board_squares[0] == board_squares[4] && board_squares[4] == board_squares[8]) {
         return false;
     } else if (board_squares[2] == board_squares[4] && board_squares[4] == board_squares[6]) {
         return false;
