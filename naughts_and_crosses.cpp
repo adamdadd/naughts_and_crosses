b2b34c74c7ec0b8002d87842037b06071bc53e32
Adam Dad <adam-dad@hotmail.co.uk>
2019-01-06 21:14:33 +0000

    v0.1.1

--- naughts_and_crosses.cpp
+++ naughts_and_crosses.cpp
@@ -22,12 +22,13 @@ int main() {
     int run = 1;
 
     draw_quantum_board(qsquares);
-    std::cout << "\n     NAUGHTS AND CROSSES\n\n";
+    std::cout << "\n      NAUGHTS AND CROSSES\n\n";
     draw_board(squares);
     play_game(squares, counter, run);
 
     return 0;
 }
+
 
 void play_game(std::vector<std::string> &board_squares, int counter, int run) {
     const std::string cross = "[X]";
@@ -55,74 +56,76 @@ void play_game(std::vector<std::string> &board_squares, int counter, int run) {
         }
 }
 
-void draw_quantum_board(std::vector<std::vector<std::string>> qboard) {
-// FOLLOWING BLOCK IS DISPLAY FUNCTION FOR QUANTUM BOARD
+
+void draw_quantum_board(std::vector<std::vector<std::string>> qboard) {// FOLLOWING BLOCK IS DISPLAY FUNCTION FOR QUANTUM BOARD
+// Block 1
     for (int i= 0; i <3; i = i+1) { // Rows
         for (int j = 0; j < 3; j = j+1) { // Columns
             std::cout << qboard[i][j];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
     std::cout << "\n";
     for ( int k = 0; k < 3; k = k+1) {
         for (int l = 3; l < 6; l = l+1){
             std::cout << qboard[k][l];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
     std::cout << "\n";
     for ( int m = 0; m < 3; m = m+1) {
         for (int n = 6; n < 9; n = n+1){
             std::cout << qboard[m][n];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
         std::cout << "\n\n";
-
+// Block 2
     for (int i= 3; i < 6; i = i+1) { // Rows
         for (int j = 0; j < 3; j = j+1) { // Columns
             std::cout << qboard[i][j];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
     std::cout << "\n";
     for ( int k = 3; k < 6; k = k+1) {
         for (int l = 3; l < 6; l = l+1){
             std::cout << qboard[k][l];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
     std::cout << "\n";
     for ( int m = 3; m < 6; m = m+1) {
         for (int n = 6; n < 9; n = n+1){
             std::cout << qboard[m][n];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
         std::cout << "\n\n";
-
+// Block 3
     for (int i= 6; i < 9; i = i+1) { // Rows
         for (int j = 0; j < 3; j = j+1) { // Columns
             std::cout << qboard[i][j];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
     std::cout << "\n";
     for ( int k = 6; k < 9; k = k+1) {
         for (int l = 3; l < 6; l = l+1){
             std::cout << qboard[k][l];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
     std::cout << "\n";
     for ( int m = 6; m < 9; m = m+1) {
         for (int n = 6; n < 9; n = n+1){
             std::cout << qboard[m][n];
         }
-        std::cout << " ";
+        std::cout << "  ";
     }
 // END OF QUANTUM BOARD DISPLAY
 }
+
 
 void draw_board(std::vector<std::string> board_squares) {
     std::string draw_board;
