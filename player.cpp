a23bc1155bf8235b685e13851ecb494fe5031297
Adam Dad <adam-dad@hotmail.co.uk>
2021-09-01 02:46:28 +0100

    refactor

--- player.cpp
+++ player.cpp
@@ -26,88 +26,80 @@ class Player {
                     std::cout << "ERROR: That's not a number!";
                 }
             } while (!valid);
+
         // Now for the enter validation and draw for each choice
             switch(square_choice) {
                 case 1:
                     if (check_schoice(0, board_squares, shape)) {
                         board_squares[0] = shape;
-                        return board_squares;
                     } else {
-                        draw_shape(shape, board_squares, run_number);
-                        return board_squares;
+                        draw_shape(shape, board_squares,run_number);
                     }
+                    return board_squares;
                     break;
                 case 2:
                     if (check_schoice(1, board_squares, shape)) {
                         board_squares[1] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares,run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 3:
                     if (check_schoice(2, board_squares, shape)) {
                         board_squares[2] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 4:
                     if (check_schoice(3, board_squares, shape)) {
                         board_squares[3] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 5:
                     if (check_schoice(4, board_squares, shape)) {
                         board_squares[4] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 6:
                     if (check_schoice(5, board_squares, shape)) {
                         board_squares[5] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 7:
                     if (check_schoice(6, board_squares, shape)) {
                         board_squares[6] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 8:
                     if (check_schoice(7, board_squares, shape)) {
                         board_squares[7] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 case 9:
                     if (check_schoice(8, board_squares, shape)) {
                         board_squares[8] = shape;
-                        return board_squares;
                     } else {
                         draw_shape(shape, board_squares, run_number);
-                        return board_squares;
                     }
+                    return board_squares;
                     break;
                 default: // If anything other than a number from 1 to 9 is entered.
                     std::cout << "ERROR: Not on the board, try again!\n";
@@ -115,6 +107,7 @@ class Player {
                     return board_squares;
                     break;
             }
+
         }
 
         bool check_schoice(int choice, std::vector<std::string> board_squares, std::string shape) {
