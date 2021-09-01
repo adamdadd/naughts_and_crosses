e8a881f6505051c950bfd686fee65df0e22dda13
Adam Dad <adam.dad@thehutgroup.com>
2021-09-01 01:58:22 +0100

    refactor

---
+++ board.hpp
@@ -0,0 +1,3 @@
+#include <vector> 
+
+const void draw_board(std::vector<std::string> board_squares);
---
+++ computer.cpp
@@ -0,0 +1,14 @@
+#include "computer.hpp"
+
+class Computer {
+    public:
+
+        int random_move() {
+            int n = 0;
+            std::random_device rd;  //Will be used to obtain a seed for the random number engine
+            std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+            std::uniform_int_distribution<> dis(1, 9);
+            n = dis(gen);
+            return n;
+        }
+};
